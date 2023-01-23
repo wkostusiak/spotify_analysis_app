2cd747ab661cd55e42cc529c344e2d57f6b70f11
wkostusiak <wkostusiak@gmail.com>
2023-01-23 22:50:04 +0100

    final commit

--- spotifystats/spotifystatsapp/functions.py
+++ spotifystats/spotifystatsapp/functions.py
@@ -9,32 +9,35 @@ import numpy as np
 from plotly.subplots import make_subplots
 from sklearn.linear_model import LinearRegression
 
+
 # top 10 artists, function returns wordcloud with their names #
 
 def top10artists(current_sp):
     artists = current_sp.current_user_top_artists(limit=10, time_range='long_term')
     names = []
-    list = artists['items']
-    for item in list:
+    artlist = artists['items']
+    for item in artlist:
         names.append(item['name'])
 
-    colors = [plotly.colors.sequential.algae[random.randrange(1, 10)] for i in range(10)]
-    weights = [random.randint(15, 35) for i in range(10)]
+    colors = [plotly.colors.sequential.algae[random.randrange(1, 10)] for _ in range(10)]
+    weights = [random.randint(15, 35) for _ in range(10)]
 
-    data = go.Scatter(x=[random.random() for i in range(30)],
-                      y=[random.random() for i in range(30)],
+    data = go.Scatter(x=[random.random() for _ in range(30)],
+                      y=[random.random() for _ in range(30)],
                       mode='text',
                       text=names,
                       marker={'opacity': 0.3},
                       textfont={'size': weights,
-                                'color': colors},)
+                                'color': colors}, )
     layout = go.Layout({'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
-                        'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}},)
+                        'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}}, )
     fig = go.Figure(data=[data], layout=layout)
 
-    fig.update_layout(
-        autosize=True,
-    )
+    fig.update_layout(autosize=True,
+                      showlegend=False,
+                      margin=dict(t=0, b=0, l=0, r=0)
+                      )
+
     names_cloud = plot(fig, output_type='div')
 
     return names_cloud
@@ -47,23 +50,23 @@ def top10artists(current_sp):
 def top10genres(current_sp):
     artists = current_sp.current_user_top_artists(limit=10, time_range='long_term')
     genres = []
-    list = artists['items']
-    for item in list:
-        if item['genres'] == []:  #  if artist is not yet classified, the array is empty #
+    genreslist = artists['items']
+    for item in genreslist:
+        if not item['genres']:  # if artist is not yet classified, the array is empty #
             pass
         else:
             genres.append(item['genres'])
 
     genres_dict = {}
     for types in genres:
-        for type in types:
-            if type not in genres_dict.keys():
-                genres_dict[type] = 1
+        for genretype in types:
+            if genretype not in genres_dict.keys():
+                genres_dict[genretype] = 1
             else:
-                genres_dict[type] = genres_dict[type] + 1
+                genres_dict[genretype] = genres_dict[genretype] + 1
 
-
-    df = pd.DataFrame({"genres": genres_dict.keys(), "genres distribution": genres_dict.values(), "genres names": genres_dict.keys() })
+    df = pd.DataFrame(
+        {"genres": genres_dict.keys(), "genres distribution": genres_dict.values(), "genres names": genres_dict.keys()})
     fig = px.bar(
         data_frame=df,
         y="genres",
@@ -74,15 +77,15 @@ def top10genres(current_sp):
     fig.update_layout(
         autosize=True,
         title={
-            'text' : 'Genres distribution for your top 10 artists',
+            'text': '<b>Genres distribution for your top 10 artists</b>',
             'y': 0.95,
             'x': 0.5,
-            'xanchor' : 'center',
-            'yanchor' : 'top'
+            'xanchor': 'center',
+            'yanchor': 'top'
         },
-        font_color = 'black',
+        font_color='black',
     )
-    fig.update_traces(marker_color='#1DB954',)
+    fig.update_traces(marker_color='#1DB954', )
 
     genres_bar_div = plot(fig, output_type='div')
 
@@ -92,76 +95,71 @@ def top10genres(current_sp):
 # top 50 tracks in long term and short term and their features (based on songs' ids) #
 # returns comparative bar chart#
 
-class TopSongs:
-    pass
-    def top_50_songs(self, current_sp, term):
-        tracks = current_sp.current_user_top_tracks(limit=50, offset=0, time_range=term)
+def top_50_songs(current_sp, term):
+    tracks = current_sp.current_user_top_tracks(limit=50, offset=0, time_range=term)
 
-        return tracks
-
-    def top_50_features_ids(self, tracks, current_sp):
-        ids = []
-        titleslist = tracks['items']
-        for item in titleslist:
-            ids.append(item['id'])
-
-        return ids
+    return tracks
 
 
-    def top_50_features(self, current_sp, ids):
+def top_50_features_ids(tracks):
+    ids = []
+    titleslist = tracks['items']
+    for item in titleslist:
+        ids.append(item['id'])
 
-        danceability = []
-        energy = []
-        speechiness = []
-        acousticness = []
-        instrumentalness = []
-        liveness = []
-        valence = []
+    return ids
 
-        tracks_features = current_sp.audio_features(ids)
-        for item in tracks_features:
-            danceability.append(float(item['danceability']))
-            energy.append(float(item['energy']))
-            speechiness.append(float(item['speechiness']))
-            acousticness.append(float(item['acousticness']))
-            instrumentalness.append(float(item['instrumentalness']))
-            liveness.append(float(item['liveness']))
-            valence.append(float(item['valence']))
 
-        list_of_features = [danceability, energy, speechiness, acousticness, instrumentalness, liveness, valence]
-        labels = ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence']
+def top_50_features(current_sp, ids):
+    danceability = []
+    energy = []
+    speechiness = []
+    acousticness = []
+    instrumentalness = []
+    liveness = []
+    valence = []
 
-        return list_of_features, labels
+    tracks_features = current_sp.audio_features(ids)
+    for item in tracks_features:
+        danceability.append(float(item['danceability']))
+        energy.append(float(item['energy']))
+        speechiness.append(float(item['speechiness']))
+        acousticness.append(float(item['acousticness']))
+        instrumentalness.append(float(item['instrumentalness']))
+        liveness.append(float(item['liveness']))
+        valence.append(float(item['valence']))
 
-    def average(self, list_of_features):
+    list_of_features = [danceability, energy, speechiness, acousticness, instrumentalness, liveness, valence]
+    labels = ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence']
 
-        avgs = []
-        for list in list_of_features:
-            value_sum = 0
-            for value in list:
-                value_sum = + value
-            avg = value_sum / len(list_of_features)
-            avgs.append(avg)
+    return list_of_features, labels
 
-        return avgs
+
+def average(list_of_features):
+    avgs = []
+    for avglist in list_of_features:
+        value_sum = 0
+        for value in avglist:
+            value_sum = + value
+        avg = value_sum / len(list_of_features)
+        avgs.append(avg)
+
+    return avgs
 
 
 def topsongs_compared(current_sp):
-
-    songs = TopSongs()
-
     # fetching data for both long and short term using TopSongs class #
-    tracks_long_term = TopSongs.top_50_songs(songs, current_sp, 'long_term')
-    tracks_short_term = TopSongs.top_50_songs(songs, current_sp, 'short_term')
+    tracks_long_term = top_50_songs(current_sp, 'long_term')
+    tracks_short_term = top_50_songs(current_sp, 'short_term')
 
-    ids_long = TopSongs.top_50_features_ids(songs, tracks_long_term, current_sp)
-    ids_short = TopSongs.top_50_features_ids(songs, tracks_short_term, current_sp)
+    ids_long = top_50_features_ids(tracks_long_term)
+    ids_short = top_50_features_ids(tracks_short_term)
 
-    list_of_features_long, labels = TopSongs.top_50_features(songs, current_sp, ids_long)
-    list_of_features_short, labels= TopSongs.top_50_features(songs, current_sp, ids_short)
+    list_of_features_long, labels = top_50_features(current_sp, ids_long)
+    list_of_features_short, labels = top_50_features(current_sp, ids_short)
 
-    avgs_long = TopSongs.average(songs, list_of_features_long)
-    avgs_short = TopSongs.average(songs, list_of_features_short)
+    avgs_long = average(list_of_features_long)
+    avgs_short = average(list_of_features_short)
 
     df = pd.DataFrame({"features": labels, "long term": avgs_long,
                        "short term": avgs_short})
@@ -178,14 +176,13 @@ def topsongs_compared(current_sp):
         labels={
             "value": "average",
             "features": "features",
-            "variable" : "term"
         },
     )
 
     fig.update_layout(
         autosize=True,
         title={
-            'text': 'Averages for different features in long and short term',
+            'text': '<b>Averages for different features in long and short term</b>',
             'y': 0.95,
             'x': 0.5,
             'xanchor': 'center',
@@ -202,35 +199,37 @@ def topsongs_compared(current_sp):
     tracks_world_dict = tracks_world['tracks']
     listofdicts = tracks_world_dict['items']
     ids_world = []
-    for dict in listofdicts:
-        nested_dict = dict['track']
+    for songdict in listofdicts:
+        nested_dict = songdict['track']
         ids_world.append(nested_dict['id'])
 
-    list_of_features_world, labels = TopSongs.top_50_features(songs, current_sp, ids_world)
-    avgs_world = TopSongs.average(songs, list_of_features_world)
+    list_of_features_world, labels = top_50_features(current_sp, ids_world)
+    avgs_world = average(list_of_features_world)
 
-    trace_short = plotly.graph_objs.Scatter(
-        x=labels,
-        y=avgs_short,
-        mode='markers',
-        name='trace short',
-        marker_color='#1DB954'
+    scatter_fig = make_subplots(rows=1, cols=1, y_title='average')
+
+    scatter_fig.add_trace(go.Scatter(
+        x=labels, y=avgs_short, mode='markers', name='you', marker=dict(color='#1DB954', size=15)),
+        row=1, col=1)
+
+    scatter_fig.add_trace(go.Scatter(
+        x=labels, y=avgs_world, mode='markers', name='world', marker=dict(color='#191414', size=15)),
+        row=1, col=1)
+
+    scatter_fig.update_layout(
+        autosize=True,
+        title={
+            'text': '<b>Average score for each feature - top 50 songs</b>',
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        font_color='black',
     )
 
-    trace_world = plotly.graph_objs.Scatter(
-        x=labels,
-        y=avgs_world,
-        mode='markers',
-        name='trace world',
-        marker_color='#191414'
-    )
-
-    data = [trace_short, trace_world]
-    fig_world = plot(data, output_type='div')
-
+    fig_world = plot(scatter_fig, output_type='div')
 
     return bar_div, fig_world
-
 
 
 # top 50 tracks in long term and decades they were released (based on songs' ids) #
@@ -247,7 +246,7 @@ def topdecades(current_sp, term):
 
     dates = []
 
-    for item in titleslist:
+    for _ in titleslist:
         for dic in titleslist:
             dates.append(dic['album']['release_date'])
 
@@ -272,61 +271,73 @@ def topdecades(current_sp, term):
     decades_vals = list(decades_dict.values())
 
     return decades_labels, decades_vals
-def topdecades_chart(current_sp):
 
+
+def topdecades_chart(current_sp):
     decades_labels, decades_vals_long = topdecades(current_sp, 'long_term')
     decades_labels, decades_vals_short = topdecades(current_sp, 'short_term')
 
     fig = make_subplots(rows=1, cols=2, shared_xaxes=False, specs=[[{"type": "pie"}, {"type": "pie"}]],
-                        subplot_titles=('top 50 songs in long term <br>', 'top 50 songs in short term <br>'))
+                        subplot_titles=('all times <br>', 'last month <br>'))
 
     fig.add_trace(go.Pie(
-        values = decades_vals_long,
-        labels= decades_labels),
+        values=decades_vals_long,
+        labels=decades_labels),
         row=1, col=1)
 
     fig.add_trace(go.Pie(
-        values = decades_vals_short,
-        labels = decades_labels),
+        values=decades_vals_short,
+        labels=decades_labels),
         row=1, col=2)
-
 
     colorscale = ['#1db954', '#EDEF5D', '#179443', '#0F757A', '#116F32', '#0E5C29', '#1db954',
                   '#33C065', '#4AC776', '#60CE87', '#77D598', '#8EDCA9']
-
 
     fig.update_traces(hoverinfo="label+percent+name",
                       marker=dict(colors=colorscale)
                       )
 
+    fig.update_annotations(yshift=20)
 
+    fig.update_layout(
+        autosize=True,
+        title={
+            'text': '<b>Predominant decades in your top 50 songs</b>',
+            'y': 0.95,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        font_color='black')
 
     donut_div = plot(fig, output_type='div')
 
     return donut_div
 
+
 # addtarce allows to add multiple scatter subplots with linear regression #
 
 def addtrace(fig, x, y, row, col):
-    fig.add_trace(go.Scatter(x=x, y=y, mode='markers', marker=dict(color='#1DB954')),row=row, col=col)
+    fig.add_trace(go.Scatter(x=x, y=y, mode='markers', marker=dict(color='#1DB954')), row=row, col=col)
     model = LinearRegression().fit(np.array(x).reshape((-1, 1)), np.array(y))
     y_pred = model.predict(np.array(x).reshape((-1, 1)))
     fig.add_trace(go.Scatter(x=x, y=y_pred, mode='lines', line=dict(color='#191414')),
-                   row, col)
+                  row, col)
+    r = model.score(X=(np.array(x).reshape(-1, 1)), y=np.array(y))
+
+    return r
+
 
 def features_compared(current_sp):
-
     # fetching features for top 50 songs #
 
-    songs = TopSongs()
-    tracks_long_term = TopSongs.top_50_songs(songs, current_sp, 'long_term')
-    ids_long = TopSongs.top_50_features_ids(songs, tracks_long_term, current_sp)
-    list_of_features_long, labels = TopSongs.top_50_features(songs, current_sp, ids_long)
+    tracks_long_term = top_50_songs(current_sp, 'long_term')
+    ids_long = top_50_features_ids(tracks_long_term)
+    list_of_features_long, labels = top_50_features(current_sp, ids_long)
 
     # scatter plot and correlation trendline - each feature vs valence #
 
-    scatter_figs = make_subplots(rows=2, cols=3, y_title='valence', vertical_spacing = 0.25)
-
+    scatter_figs = make_subplots(rows=2, cols=3, y_title='valence', vertical_spacing=0.25)
 
     # adding multiple subplots using addtrace function #
 
@@ -334,21 +345,27 @@ def features_compared(current_sp):
     for feature in list_of_features_long[:-1]:
         if i < 4:
             row = 1
-            addtrace(fig=scatter_figs, x=feature, y=list_of_features_long[6],
-                     row=row, col=i)
-            scatter_figs.update_xaxes(title_text=f'{labels[i-1]}', row=row, col=i)
+            r = addtrace(fig=scatter_figs, x=feature, y=list_of_features_long[6],
+                         row=row, col=i)
+            scatter_figs.update_xaxes(title_text=f'{labels[i - 1]} R\u00b2 = {r:.2f}', row=row, col=i)
         else:
             row = 2
-            addtrace(fig=scatter_figs, x=feature, y=list_of_features_long[6],
-                     row=row, col=i-3,)
-            scatter_figs.update_xaxes(title_text=f'{labels[i-1]} <br>', row=row, col=i-3)
+            r = addtrace(fig=scatter_figs, x=feature, y=list_of_features_long[6],
+                         row=row, col=i - 3, )
+            scatter_figs.update_xaxes(title_text=f'{labels[i - 1]} R\u00b2 = {r:.2f}', row=row, col=i - 3)
         i += 1
 
     scatter_figs.update_layout(
         autosize=True,
+        title={
+            'text': '<b>Valence score vs other features</b>',
+            'y': 0.95,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
         font=dict(color='black'),
-        showlegend=False
-    )
+        showlegend=False)
 
     scatter_div = plot(scatter_figs, output_type='div')
 
@@ -367,17 +384,15 @@ def features_compared(current_sp):
         y=x_list,
         hoverongaps=True,
         colorscale='aggrnyl'
-        ))
+    ))
 
     fig_heatmap.update_layout(
         autosize=True,
         title={
-            'text': 'Correlation heatmap for features in your top 50 songs',
-            'y': 0.95,
+            'text': '<b>Correlation heatmap for features in your top 50 songs</b>',
             'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top',
-
         },
         font_color='black',
     )
@@ -385,6 +400,3 @@ def features_compared(current_sp):
     heat_div = plot(fig_heatmap, output_type='div')
 
     return heat_div, scatter_div
-
-
-
--- spotifystats/spotifystatsapp/views.py
+++ spotifystats/spotifystatsapp/views.py
@@ -1,38 +1,28 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from rest_framework.views import APIView
 from decouple import config
-import pandas as pd
-import requests
-from requests import Request, post
-from rest_framework import status
-from rest_framework.response import Response
-from django .utils import timezone
-from datetime import timedelta
-from django.shortcuts import redirect
-
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from .functions import top10artists, top10genres, topdecades_chart, topsongs_compared, features_compared
 
-from .functions import top10artists, top10genres,  topdecades_chart, topsongs_compared, features_compared
 
 class WelcomePage(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'spotifystatsapp/welcome.html')
 
+
 class Congrats(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'spotifystatsapp/congrats.html')
 
+
 class AuthURL(View):
     def get(self, request, *args, **kwargs):
-        sp_auth=spotipy.oauth2.SpotifyOAuth(
-        client_id = config('CLIENT_ID'),
-        client_secret = config('CLIENT_SECRET'),
-        redirect_uri='http://127.0.0.1:8000/congrats',
-        scope='playlist-read-private user-top-read user-library-read',
-        open_browser=True,
+        sp_auth = spotipy.oauth2.SpotifyOAuth(
+            client_id=config('CLIENT_ID'),
+            client_secret=config('CLIENT_SECRET'),
+            redirect_uri='http://127.0.0.1:8000/congrats',
+            scope='playlist-read-private user-top-read user-library-read',
+            open_browser=True,
         )
         sp_auth.get_access_token(request.GET.get("code"))
         sp = spotipy.client.Spotify(auth_manager=sp_auth)
@@ -42,22 +32,18 @@ class AuthURL(View):
         names_cloud = top10artists(sp)
         genres_bar_div = top10genres(sp)
         donut_div = topdecades_chart(sp)
-        bar_div,fig_world = topsongs_compared(sp)
+        bar_div, fig_world = topsongs_compared(sp)
         heat_div, scatter_div = features_compared(sp)
 
-
-
         context = {
-            'genres_bar_div' : genres_bar_div,
-            'names_cloud' : names_cloud,
-            'bar_div' : bar_div,
-            'donut_div' : donut_div,
-            'heat_div' : heat_div,
-            'fig_world' : fig_world,
+            'genres_bar_div': genres_bar_div,
+            'names_cloud': names_cloud,
+            'bar_div': bar_div,
+            'donut_div': donut_div,
+            'heat_div': heat_div,
+            'fig_world': fig_world,
             'scatter_div': scatter_div,
 
         }
 
         return render(request, 'spotifystatsapp/congrats.html', context)
-
-
